5565c377e89200290a4379d9b5be86ab2b153956
imabhishek02 <abhishekrohit369@gmail.com>
2023-04-14 17:23:51 +0530

    Updating experience section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Navbar from "./components/Navbar/Navbar";
 import './App.css';
 import Intro from "./components/Intro/Intro";
 import Projects from "./components/Projects/services"
+import Experience from "./components/Experience/Experience"
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Intro/>
       <Projects/>
+      <Experience/>
     </div>
   );
 }
---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Experience.css'
+const Experience = () => {
+  return (
+    <div className="experience">
+        <div className="achievement">
+            <div className="circle">20+</div>
+            <span>Frontend</span>
+            <span>Projects</span>
+        </div>
+        <div className="achievement">
+            <div className="circle">500+</div>
+            <span>Coding</span>
+            <span>Questions</span>
+            <span className='s3'>(Solved)</span>
+        </div>
+        <div className="achievement">
+            <div className="circle">5+</div>
+            <span>Certified</span>
+            <span>Achievements</span>
+        </div>
+    </div>
+  )
+}
+
+export default Experience
+
